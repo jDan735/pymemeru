e52d718b17ce770d012f86bae61a148e442c35ed
Daniel Zakharov <gzdan734@gmail.com>
2025-11-01 18:43:47 +0300

    fixes, new lxml,  uv, ruff

--- pymemeru/__init__.py
+++ pymemeru/__init__.py
@@ -1,8 +1,7 @@
-try:
-    from .page import page
-    from .search import search
-    from .popular import popular
-except Exception:
-    pass
 
-__version__ = "0.1.0"
+from .page import page
+from .search import search
+from .popular import popular
+
+__version__ = "0.2.4"
+__all__ = ("page", "search", "popular")
--- tests/test_page.py
+++ tests/test_page.py
@@ -14,7 +14,7 @@ class TestPage:
         assert page.title == "Арестованный Виктор Медведчук"
         assert page.published_at == "13.04.2022 в 09:12"
 
-        assert page.author_name == "Настя Кривец"
+        assert page.author_name == "Mememother"
         
         assert page.main_image == "https://memepedia.ru/wp-content/uploads/2022/04/photo_2022-04-13_09-29-35.jpg"
         assert page.text != ""
